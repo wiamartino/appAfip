c2d1463039c8f387ee34a01170f1a6762e9ff17a
Walter Iamartino <walteriamartino@gmail.com>
2023-05-21 20:01:27 -0300

    error handling

--- routes/index.js
+++ routes/index.js
@@ -8,60 +8,71 @@ router.get('/', (req, res) => {
 });
 
 router.post('/getCerts', (req, res) => {
-  const keys = forge.pki.rsa.generateKeyPair(2048);
-  const csr = forge.pki.createCertificationRequest();
-  const cuit = 'CUIT ' + req.body.param_snumber;
+  try {
+    const keys = forge.pki.rsa.generateKeyPair(2048);
+    const csr = forge.pki.createCertificationRequest();
+    const cuit = 'CUIT ' + req.body.param_snumber;
 
-  csr.publicKey = keys.publicKey;
+    csr.publicKey = keys.publicKey;
 
-  csr.setSubject([
-    { name: 'countryName', value: req.body.param_c, type: 'countryName' },
-    { name: 'organizationName', value: req.body.param_o, type: 'organizationName' },
-    { name: 'commonName', value: req.body.param_cn, type: 'commonName' },
-    { name: 'serialName', value: cuit, type: 'serialName' }
-  ]);
+    csr.setSubject([
+      { name: 'countryName', value: req.body.param_c, type: 'countryName' },
+      { name: 'organizationName', value: req.body.param_o, type: 'organizationName' },
+      { name: 'commonName', value: req.body.param_cn, type: 'commonName' },
+      { name: 'serialName', value: cuit, type: 'serialName' }
+    ]);
 
-  csr.sign(keys.privateKey);
+    csr.sign(keys.privateKey);
 
-  const pem = forge.pki.certificationRequestToPem(csr);
-  const privadapem = forge.pki.privateKeyToPem(keys.privateKey);
+    const pem = forge.pki.certificationRequestToPem(csr);
+    const privadapem = forge.pki.privateKeyToPem(keys.privateKey);
 
-  const archive = archiver('zip');
+    const archive = archiver('zip');
 
-  archive.on('error', (err) => {
-    res.status(500).send({ error: err.message });
-  });
+    archive.on('error', (err) => {
+      console.error('An error occurred while creating the archive:', err);
+      res.status(500).send({ error: err.message });
+    });
 
-  archive.on('end', () => {
-    console.log('Archive wrote %d bytes', archive.pointer());
-  });
+    archive.on('end', () => {
+      console.log('Archive wrote %d bytes', archive.pointer());
+    });
 
-  const nombreZip = `certificados - ${req.body.param_o}.zip`;
+    const nombreZip = `certificados - ${req.body.param_o}.zip`;
 
-  res.attachment(nombreZip);
-  archive.pipe(res);
-  archive.append(privadapem, { name: 'privada.key' });
-  archive.append(pem, { name: 'certificado.csr' });
-  archive.finalize();
+    res.attachment(nombreZip);
+    archive.pipe(res);
+    archive.append(privadapem, { name: 'privada.key' });
+    archive.append(pem, { name: 'certificado.csr' });
+    archive.finalize();
+  } catch (error) {
+    console.error('An error occurred:', error);
+    res.status(500).send('Internal Server Error');
+  }
 });
 
 router.post('/getP12', (req, res) => {
-  const privateKey = forge.pki.privateKeyFromPem(req.files.privada.data.toString());
-  const cert = forge.pki.certificateFromPem(req.files.certCRT.data.toString());
+  try {
+    const privateKey = forge.pki.privateKeyFromPem(req.files.privada.data.toString());
+    const cert = forge.pki.certificateFromPem(req.files.certCRT.data.toString());
 
-  const p12Asn1 = forge.pkcs12.toPkcs12Asn1(
-    privateKey, cert, req.body.passp12,
-    { friendlyName: req.body.userp12, algorithm: '3des' }
-  );
+    const p12Asn1 = forge.pkcs12.toPkcs12Asn1(
+      privateKey, cert, req.body.passp12,
+      { friendlyName: req.body.userp12, algorithm: '3des' }
+    );
 
-  const p12Der = forge.asn1.toDer(p12Asn1).getBytes();
-  const p12b64 = forge.util.encode64(p12Der);
-  const nombreArchivo = `${req.body.userp12}+store.p12`;
-  const archivo = Buffer.from(p12b64, 'base64');
+    const p12Der = forge.asn1.toDer(p12Asn1).getBytes();
+    const p12b64 = forge.util.encode64(p12Der);
+    const nombreArchivo = `${req.body.userp12}+store.p12`;
+    const archivo = Buffer.from(p12b64, 'base64');
 
-  res.setHeader('Content-type', 'application/octet-stream');
-  res.setHeader('Content-Disposition', `filename=${nombreArchivo}`);
-  res.send(archivo);
+    res.setHeader('Content-type', 'application/octet-stream');
+    res.setHeader('Content-Disposition', `filename=${nombreArchivo}`);
+    res.send(archivo);
+  } catch (error) {
+    console.error('An error occurred:', error);
+    res.status(500).send('Internal Server Error');
+  }
 });
 
 module.exports = router;
